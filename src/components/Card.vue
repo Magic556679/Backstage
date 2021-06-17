96b608ffcf1776d3753989c1dbaf2af0fcda0a45
李晏瑋 <magic556679@gmail.com>
2021-06-17 14:06:53 +0800

    Card.vue 第二次更新

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="card" style="width: 18rem;">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">{{ title }}</h5>
             <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             <a href="#" class="card-link">Card link</a>
@@ -9,3 +9,13 @@
         </div>
     </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'eslint好難用'
+    }
+  }
+}
+</script>
